72e98f9f8df9e1543c986ca30df53aa32bd5a776
GabrielDragone <37886654+GabrielDragone@users.noreply.github.com>
2019-10-12 15:47:08 -0300

    :dog: adc examples for: images, buttons and descriptions

--- src/flutter_essential/lib/home_page.dart
+++ src/flutter_essential/lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,8 +21,30 @@ class HomePage extends StatelessWidget {
     return Container(
       color: Colors.white,
       child: Center(
-        child: _text(),
+        child: _button(),
       ),
+    );
+  }
+
+  _button() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text(
+        "Olá, mundo!",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      onPressed: () => _onClickOk(),
+    );
+  }
+
+  void _onClickOk() => print("Olá");
+
+  _image() {
+    return Image.asset(
+      "assets/images/dog4.png",
+      fit: BoxFit.contain,
     );
   }
 
@@ -39,3 +63,37 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+//Explicações:
+// Child: Utilizados por Widgets que suportam apenas 1 filho, exemplo CENTER.
+// Childres: Utilizados por Widgets que podem receber mais de um filho, exemplo ListView.
+
+//Atalhos:
+// Cntrl + Alt + M = Cria um method utilizando seleção feita pelo dev.
+// Cntrl + Alt + L = Auto indent
+
+//Imagens:
+// Para utilizar as imagens do assets, editar em pubspec.yaml:
+/*  # To add assets to your application, add an assets section, like this:
+  assets:
+    - assets/images/ #para utilizar as imagens do assets
+* */
+//fit: pra dimensionar as imagens
+//SizedBox.expand: Preenche toodo o espaço do container pai pra ocupar a imagem na tela inteira, vai no lugar do center, dps utilizar fit
+
+//RaisedButton:
+// Sempre colocar o onPressed(). E esse dentro desse estiver null, o botão fica desabilitado.
+// A descrição do botão sempre irá dentro de um child: Text("Botão").
+
+//Collumn e Row:
+// Column: Vertical ou em baixo do outro.
+// Row: Horizontalmente, lado a lado.
+// Ambos vão no lugar do Center e tem children (filhos).
+//MainAxisSize: Define o tamanho da coluna ou linha,
+
+//MainAxisAlignment:
+// Alinha a coluna ou linha no eixo principal.
+// Define onde os childrens ficaram posicionados, variando entre start(padrão), center ou end.
+
+//CrossAxisAlignment:
+// Alinha a coluna ou linha no eixo cruzado.
